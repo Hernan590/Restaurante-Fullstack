755fc1c9e58f389e7578d9075ea945f535a13d24
Hernan590 <herjosuee@hotmail.com>
2024-12-06 22:01:09 -0500

    Prueba

--- backend/routes/routerInicioSesion.js
+++ backend/routes/routerInicioSesion.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const validate = require('../controllers/inicioSesion');
-
+//prueba
 router.post('/validar/:username', validate.validateUser);
 
 module.exports = router;
